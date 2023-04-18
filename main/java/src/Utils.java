c16c400d256c07320e7b2c83df8d582cf1bcf9d4
ronantremoureux <ronan.tremoureux@gmail.com>
2023-04-18 10:57:27 +0200

    modifying ui

--- main/java/src/Utils.java
+++ main/java/src/Utils.java
@@ -5,6 +5,11 @@
  */
 public class Utils {
 
+  /**
+   * Print a message if the debug mode is on in the SETTINGS.
+   *
+   * @param s The message to print
+   */
   public static void printDebug(String s) {
     if (SETTINGS.DEBUG) {
       System.out.println("ImageFilteringEngine_DEBUG |> " + s);
